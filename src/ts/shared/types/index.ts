7c93154be3e289e77ee49803c00e339d7df086c1
Shawn McKnight <smcknight@storis.com>
2022-02-23 16:02:14 -0500

    Relocate GenericObject definition

--- src/ts/shared/types/index.ts
+++ src/ts/shared/types/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type GenericObject = Record<string, any>;
+
 export type ValidationFunction = () => boolean | Promise<boolean>;
 
 export type SchemaValidator = ValidationFunction | [ValidationFunction, string];
