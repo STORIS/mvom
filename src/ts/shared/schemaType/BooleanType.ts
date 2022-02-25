c4b36c6aad2136fb5b9bccc550dde7cd765ae241
Shawn McKnight <smcknight@storis.com>
2022-02-25 10:05:33 -0500

    Convert BooleanType

---
+++ src/ts/shared/schemaType/BooleanType.ts
@@ -0,0 +1,31 @@
+import ScalarType from './ScalarType';
+
+/** Boolean Schema Type */
+class BooleanType extends ScalarType {
+	/** Transform mv style data to Boolean */
+
+	public transformFromDb(value: unknown): boolean {
+		// this logic is intentionally trying to mimic the Boolean rules of the UniBasic interpreter
+		return value != null && value !== '0' && value !== 0;
+	}
+
+	/** Transform js style data to mv style data */
+	public transformToDb(value: unknown): '1' | '0' {
+		return value ? '1' : '0';
+	}
+
+	/** Transform query constants to u2 formatted Boolean */
+	public override transformToQuery(value: unknown): '1' | '0' | unknown {
+		if (value === true || value === 'true' || value === 'TRUE') {
+			return '1';
+		}
+
+		if (value === false || value === 'false' || value === 'FALSE') {
+			return '0';
+		}
+
+		return value;
+	}
+}
+
+export default BooleanType;
