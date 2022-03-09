84ee70453db52866b00357c0c0b19b6577a54747
Shawn McKnight <smcknight@storis.com>
2022-03-09 15:51:52 -0500

    Export additional types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 export { default as Connection } from './Connection';
 export { default as Document } from './Document';
-export { default as Schema } from './Schema';
-export * as Errors from './errors';
+export { default as Schema, type SchemaDefinition, type SchemaConstructorOptions } from './Schema';
+export type { ModelConstructor } from './compileModel';
+export {
+	ConnectionManagerError,
+	DataValidationError,
+	DbServerError,
+	ForeignKeyValidationError,
+	InvalidParameterError,
+	InvalidServerFeaturesError,
+	RecordLockedError,
+	RecordVersionError,
+	TransformDataError,
+} from './errors';
+export type {
+	SchemaTypeDefinitionBoolean,
+	SchemaTypeDefinitionISOCalendarDateTime,
+	SchemaTypeDefinitionISOCalendarDate,
+	SchemaTypeDefinitionISOTime,
+	SchemaTypeDefinitionNumber,
+	SchemaTypeDefinitionString,
+} from './schemaType';
+export type { EncryptFunc, DecryptFunc } from './types';
--- src/schemaType/index.ts
+++ src/schemaType/index.ts
@@ -3,12 +3,18 @@ export { default as BaseDateType } from './BaseDateType';
 export { default as BaseScalarArrayType } from './BaseScalarArrayType';
 export { default as BaseScalarType, type SchemaTypeDefinitionScalar } from './BaseScalarType';
 export { default as BaseSchemaType } from './BaseSchemaType';
-export { default as BooleanType } from './BooleanType';
+export { default as BooleanType, type SchemaTypeDefinitionBoolean } from './BooleanType';
 export { default as DocumentArrayType } from './DocumentArrayType';
 export { default as EmbeddedType } from './EmbeddedType';
-export { default as ISOCalendarDateTimeType } from './ISOCalendarDateTimeType';
-export { default as ISOCalendarDateType } from './ISOCalendarDateType';
-export { default as ISOTimeType } from './ISOTimeType';
+export {
+	default as ISOCalendarDateTimeType,
+	type SchemaTypeDefinitionISOCalendarDateTime,
+} from './ISOCalendarDateTimeType';
+export {
+	default as ISOCalendarDateType,
+	type SchemaTypeDefinitionISOCalendarDate,
+} from './ISOCalendarDateType';
+export { default as ISOTimeType, type SchemaTypeDefinitionISOTime } from './ISOTimeType';
 export { default as NestedArrayType } from './NestedArrayType';
-export { default as NumberType } from './NumberType';
-export { default as StringType } from './StringType';
+export { default as NumberType, type SchemaTypeDefinitionNumber } from './NumberType';
+export { default as StringType, type SchemaTypeDefinitionString } from './StringType';
