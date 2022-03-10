34966ae749950146168d3c686f4022e2f7e73906
Shawn McKnight <smcknight@storis.com>
2022-03-10 17:42:08 -0500

    Relocate remaining types to misc

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,4 @@ export * from './DbFeature';
 export * from './DbSubroutineInput';
 export * from './DbSubroutineOutput';
 export * from './Encryption';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type GenericObject = Record<string, any>;
-
-export type MvRecord = (string | number | null | undefined | MvRecord)[];
+export * from './Miscellaneous';
