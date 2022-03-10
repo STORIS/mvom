2501ee4a236fb01932587d66038be58f596b5727
Shawn McKnight <smcknight@storis.com>
2022-03-10 18:11:11 -0500

    Clean up types in MvisError

--- src/errors/MvisError.ts
+++ src/errors/MvisError.ts
@@ -1,19 +1,18 @@
-import type { GenericObject } from '../types';
 import BaseError from './BaseError';
 
 interface MvisErrorConstructorOptions {
 	message?: string;
-	connectionManagerRequest?: GenericObject;
-	connectionManagerResponse?: GenericObject;
+	connectionManagerRequest?: unknown;
+	connectionManagerResponse?: unknown;
 }
 
 /** Error thrown when an error occurs when communicating with the connection manager */
 class MvisError extends BaseError {
 	/** Request object passed to connection manager */
-	public readonly connectionManagerRequest: GenericObject;
+	public readonly connectionManagerRequest: unknown;
 
 	/** Response object returned from connection manager (if any) */
-	public readonly connectionManagerResponse: GenericObject;
+	public readonly connectionManagerResponse: unknown;
 
 	public constructor({
 		message = 'Error in MVIS communication',
