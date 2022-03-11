3e9182408c8c5d05fb2942ce0e754c1f350ff80b
Shawn McKnight <smcknight@storis.com>
2022-03-11 10:29:01 -0500

    Reorder error exports

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,9 +1,9 @@
-export { default as MvisError } from './MvisError';
 export { default as DataValidationError } from './DataValidationError';
 export { default as DbServerError } from './DbServerError';
 export { default as ForeignKeyValidationError } from './ForeignKeyValidationError';
 export { default as InvalidParameterError } from './InvalidParameterError';
 export { default as InvalidServerFeaturesError } from './InvalidServerFeaturesError';
+export { default as MvisError } from './MvisError';
 export { default as RecordLockedError } from './RecordLockedError';
 export { default as RecordVersionError } from './RecordVersionError';
 export { default as TransformDataError } from './TransformDataError';
