f3a486209d07b62aab91df5d82a58c2d5dc04a72
Shawn McKnight <smcknight@storis.com>
2022-03-12 10:33:07 -0500

    Improve EncryptFn type

--- src/types/Encryption.ts
+++ src/types/Encryption.ts
@@ -1,9 +1,15 @@
 export type DecryptFn = {
 	(data: string | null): unknown | null;
 	(data: (string | null)[]): (unknown | null)[];
+	(data: (string | null)[][]): (unknown | null)[][];
 };
 
-export type EncryptFn = {
-	(data: unknown | null): string | null;
-	(data: (unknown | null)[]): (string | null)[];
-};
+export type EncryptFn = <TData extends string | null | (string | null)[] | (string | null)[][]>(
+	data: TData,
+) => TData extends string
+	? string
+	: TData extends null
+	? null
+	: TData extends (string | null)[]
+	? (string | null)[]
+	: (string | null)[][];
