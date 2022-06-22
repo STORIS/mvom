7a8b11a135b9b4590b330bcfd1b41b7754696cf3
Shawn McKnight <smcknight@storis.com>
2022-06-22 18:38:57 -0400

    Fix InvalidServerFeaturesError tests

--- src/errors/__tests__/InvalidServerFeaturesError.test.ts
+++ src/errors/__tests__/InvalidServerFeaturesError.test.ts
@@ -1,13 +1,10 @@
 import InvalidServerFeaturesError from '../InvalidServerFeaturesError';
 
-const invalidFeatures = ['invalid1', 'invalid2'];
-
 test('should instantiate error with expected instance properties', (): void => {
-	const error = new InvalidServerFeaturesError({ invalidFeatures });
+	const error = new InvalidServerFeaturesError();
 	const expected = {
 		name: 'InvalidServerFeaturesError',
 		message: 'Invalid feature set on db server',
-		invalidFeatures,
 	};
 	expect(error).toMatchObject(expected);
 });
@@ -16,7 +13,6 @@ test('should allow for override of message', (): void => {
 	const message = 'foo';
 	const error = new InvalidServerFeaturesError({
 		message,
-		invalidFeatures,
 	});
 	expect(error.message).toEqual(message);
 });
