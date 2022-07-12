24439086137d2175c09f282d00a104b3b6f86133
Shawn McKnight <smcknight@storis.com>
2022-07-12 11:52:02 -0400

    Update prettier configs [skip ci]

--- packages/website/.eslintrc.js
+++ packages/website/.eslintrc.js
@@ -1,36 +1,35 @@
-const { rules: baseRules } = require("@storis/eslint-config/nodejs");
+const { rules: baseRules } = require('@storis/eslint-config/nodejs');
 
-const baseNamingRules =
-  baseRules["@typescript-eslint/naming-convention"].slice(1);
+const baseNamingRules = baseRules['@typescript-eslint/naming-convention'].slice(1);
 
 module.exports = {
-  extends: ["@storis/eslint-config/nodejs"],
+	extends: ['@storis/eslint-config/nodejs'],
 
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.eslint.json"],
-  },
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.eslint.json'],
+	},
 
-  rules: {
-    // set up naming convention rules
-    "@typescript-eslint/naming-convention": [
-      "error",
-      // allow PascalCase functions
-      { selector: "function", format: ["camelCase", "PascalCase"] },
-      ...baseNamingRules,
-    ],
+	rules: {
+		// set up naming convention rules
+		'@typescript-eslint/naming-convention': [
+			'error',
+			// allow PascalCase functions
+			{ selector: 'function', format: ['camelCase', 'PascalCase'] },
+			...baseNamingRules,
+		],
 
-    "import/no-default-export": "off",
-    "import/prefer-default-export": "error",
-  },
+		'import/no-default-export': 'off',
+		'import/prefer-default-export': 'error',
+	},
 
-  overrides: [
-    {
-      files: ["./.*.js", "./*.js"],
-      rules: {
-        // allow requires in config files
-        "@typescript-eslint/no-var-requires": "off",
-      },
-    },
-  ],
+	overrides: [
+		{
+			files: ['./.*.js', './*.js'],
+			rules: {
+				// allow requires in config files
+				'@typescript-eslint/no-var-requires': 'off',
+			},
+		},
+	],
 };
