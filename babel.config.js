5c0fb4e299d1ae209634b068926be1df360d30ce
Shawn McKnight <smcknight@storis.com>
2022-07-14 19:09:02 -0400

    Create root babel.config.js Add root eslintrc to avoid issues with lint-staged

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	ignore: ['**/*.d.ts'],
+	env: {
+		build: {
+			ignore: ['**/*.test.ts', '**/__mocks__/**'],
+			presets: [['@babel/env', { targets: { node: '14.19.0' } }], '@babel/typescript'],
+		},
+		debug: { sourceMaps: 'inline', retainLines: true },
+	},
+	presets: [['@babel/env', { targets: { node: 'current' } }], '@babel/typescript'],
+	plugins: [['@babel/plugin-transform-runtime']],
+};
