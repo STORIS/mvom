b6b110628495f014c0fc06a741ffd010e94dd033
Ken Thompson <kat@storis.com>
2022-12-09 15:10:16 -0500

    Return write error status codes (#322)
    This change improves the reporting of write errors by returning additional error codes that map to the result of the `status()` function, which is now called in the `write` error clause.
    
    The additional error codes can now distinguish that a write failed due to a "system" error ie a permission error or file corruption, violating a trigger constraint ie the update trigger rejected the write, there was an error with the trigger, or a no duplicate index constraint was violated. An "unknown write error" code was also created in case an unknown status is returned. All four `status()` return values defined in the UniBasic reference guide were mapped.
    
    A note about the "trigger error" return code. I was able to replicate this by decataloging the trigger program. If the trigger itself aborts then this status code is not returned and UniObjects returns its own subroutine failed error code.

--- src/constants/dbErrors.ts
+++ src/constants/dbErrors.ts
@@ -19,6 +19,16 @@ const dbErrors: Record<string, DbError> = {
 	digestHash: { code: 13, message: 'Error in constructing digest hash' },
 	foreignKeyValidation: { code: 14, message: 'Foreign key validation error' },
 	fileEncodeBase64: { code: 15, message: 'Error in encoding the file as Base64 string' },
+	recordWriteTriggerConstraint: { code: 16, message: 'Record write rejected by trigger' },
+	recordWriteTriggerError: {
+		code: 17,
+		message: 'Error writing database record due to trigger error',
+	},
+	recordWriteDuplicateIndex: {
+		code: 18,
+		message: 'Record write rejected due to violating duplicate index constraint',
+	},
+	recordWriteUnknown: { code: 19, message: 'Unknown error writing database record' },
 };
 
 export default dbErrors;
