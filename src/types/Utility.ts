f3a20dda783f98236561eb1542245ffca69a93d9
Shawn McKnight <smcknight@storis.com>
2024-06-13 15:50:28 -0400

    Create tests of utility types

--- src/types/Utility.ts
+++ src/types/Utility.ts
@@ -1,2 +1,8 @@
 /** Mark some properties as required, leaving others unchanged */
 export type MarkRequired<T, RK extends keyof T> = Omit<T, RK> & Required<Pick<T, RK>>;
+
+/** Assert that two types are equal */
+export type Assert<T, U> =
+	(<V>() => V extends T ? 1 : 2) extends <V>() => V extends U ? 1 : 2
+		? true
+		: { error: 'Types are not equal'; type1: T; type2: U };
