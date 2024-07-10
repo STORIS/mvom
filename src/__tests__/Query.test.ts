ecd84ac79b53380ac59b1331d4501e615262fe7c
Shawn McKnight <smcknight@storis.com>
2024-07-10 19:04:05 -0400

    Add tests of Filter type

--- src/__tests__/Query.test.ts
+++ src/__tests__/Query.test.ts
@@ -3,10 +3,11 @@ import mockDelimiters from '#test/mockDelimiters';
 import type Connection from '../Connection';
 import { InvalidParameterError, QueryLimitError } from '../errors';
 import type LogHandler from '../LogHandler';
-import type { Filter, QueryExecutionOptions, SortCriteria } from '../Query';
+import type { Condition, Filter, QueryExecutionOptions, SortCriteria } from '../Query';
 import Query from '../Query';
+import type { ISOCalendarDate, ISOCalendarDateTime, ISOTime } from '../Schema';
 import Schema from '../Schema';
-import type { DbSubroutineOutputFind } from '../types';
+import type { DbSubroutineOutputFind, Equals } from '../types';
 
 const connectionMock = mockDeep<Connection>();
 const filename = 'filename';
@@ -1678,6 +1679,205 @@ describe('exec', () => {
 				},
 				{},
 			);
+		});
+	});
+});
+
+describe('utility types', () => {
+	describe('Filter', () => {
+		test('should allow for a filter with a single schema property', () => {
+			const schema = new Schema({ stringProp: { type: 'string', path: '1' } });
+
+			const test1: Equals<
+				Filter<typeof schema>,
+				{
+					$and?: Filter<typeof schema>[];
+					$or?: Filter<typeof schema>[];
+					stringProp?: Condition<string>;
+					_id?: Condition<string>;
+				}
+			> = true;
+			expect(test1).toBe(true);
+		});
+
+		test('should construct filter type from a single dictionary', () => {
+			const schema = new Schema({}, { dictionaries: { stringDictionary: 'STRING_DICTIONARY' } });
+			const test1: Equals<
+				Filter<typeof schema>,
+				{
+					$and?: Filter<typeof schema>[];
+					$or?: Filter<typeof schema>[];
+					stringDictionary?: Condition<string>;
+					_id?: Condition<string>;
+				}
+			> = true;
+			expect(test1).toBe(true);
+		});
+
+		test('should construct filter type from both a schema property and a dictionary', () => {
+			const schema = new Schema(
+				{ stringProp: { type: 'string', path: '1' } },
+				{ dictionaries: { stringDictionary: 'STRING_DICTIONARY' } },
+			);
+			const test1: Equals<
+				Filter<typeof schema>,
+				{
+					$and?: Filter<typeof schema>[];
+					$or?: Filter<typeof schema>[];
+					stringProp?: Condition<string>;
+					stringDictionary?: Condition<string>;
+					_id?: Condition<string>;
+				}
+			> = true;
+			expect(test1).toBe(true);
+		});
+
+		test('should construct filter type from all possible dictionary formats', () => {
+			const schema = new Schema(
+				{},
+				{
+					dictionaries: {
+						stringDictionary: 'STRING_DICTIONARY',
+						stringDictionaryExplicit: { type: 'string', dictionary: 'STRING_DICTIONARY_EXPLICIT' },
+						numberDictionary: { type: 'number', dictionary: 'NUMBER_DICTIONARY' },
+						booleanDictionary: { type: 'boolean', dictionary: 'BOOLEAN_DICTIONARY' },
+						isoCalendarDateDictionary: {
+							type: 'ISOCalendarDate',
+							dictionary: 'ISO_CALENDAR_DATE_DICTIONARY',
+						},
+						isoTimeDictionary: { type: 'ISOTime', dictionary: 'ISO_TIME_DICTIONARY' },
+						isoCalendarDateTimeDictionary: {
+							type: 'ISOCalendarDateTime',
+							dictionary: 'ISO_CALENDAR_DATE_TIME_DICTIONARY',
+						},
+					},
+				},
+			);
+			const test1: Equals<
+				Filter<typeof schema>,
+				{
+					$and?: Filter<typeof schema>[];
+					$or?: Filter<typeof schema>[];
+					stringDictionary?: Condition<string>;
+					stringDictionaryExplicit?: Condition<string>;
+					numberDictionary?: Condition<number>;
+					booleanDictionary?: Condition<boolean>;
+					isoCalendarDateDictionary?: Condition<ISOCalendarDate>;
+					isoTimeDictionary?: Condition<ISOTime>;
+					isoCalendarDateTimeDictionary?: Condition<ISOCalendarDateTime>;
+					_id?: Condition<string>;
+				}
+			> = true;
+			expect(test1).toBe(true);
+		});
+
+		test('should construct filter type from mixed schema', () => {
+			const schema = new Schema(
+				{
+					booleanOptional: { type: 'boolean', path: '1' },
+					booleanRequired: { type: 'boolean', path: '2', required: true },
+					stringOptional: { type: 'string', path: '3' },
+					stringRequired: { type: 'string', path: '4', required: true },
+					numberOptional: { type: 'number', path: '5' },
+					numberRequired: { type: 'number', path: '6', required: true },
+					isoCalendarDateOptional: { type: 'ISOCalendarDate', path: '7' },
+					isoCalendarDateRequired: { type: 'ISOCalendarDate', path: '8', required: true },
+					isoTimeOptional: { type: 'ISOTime', path: '9' },
+					isoTimeRequired: { type: 'ISOTime', path: '10', required: true },
+					isoCalendarDateTimeOptional: { type: 'ISOCalendarDateTime', path: '11' },
+					isoCalendarDateTimeRequired: { type: 'ISOCalendarDateTime', path: '12', required: true },
+					arrayOptional: [{ type: 'string', path: '13' }],
+					arrayRequired: [{ type: 'string', path: '14', required: true }],
+					nestedArrayOptional: [[{ type: 'string', path: '15' }]],
+					nestedArrayRequired: [[{ type: 'string', path: '16', required: true }]],
+					embeddedOptional: new Schema({
+						innerEmbeddedProp: { type: 'string', path: '17' },
+					}),
+					embeddedRequired: new Schema({
+						innerEmbeddedProp: { type: 'string', path: '18', required: true },
+					}),
+					documentArrayOptional: [
+						{
+							docStringProp: { type: 'string', path: '19' },
+							docNumberProp: { type: 'number', path: '20' },
+						},
+					],
+					documentArrayRequired: [
+						{
+							docStringProp: { type: 'string', path: '21', required: true },
+							docNumberProp: { type: 'number', path: '22' },
+						},
+					],
+					documentArraySchemaOptional: [
+						new Schema({
+							docStringProp: { type: 'string', path: '23' },
+						}),
+					],
+					documentArraySchemaRequired: [
+						new Schema({
+							docStringProp: { type: 'string', path: '24', required: true },
+						}),
+					],
+				},
+				{
+					dictionaries: {
+						stringDictionary: 'STRING_DICTIONARY',
+						stringDictionaryExplicit: { type: 'string', dictionary: 'STRING_DICTIONARY_EXPLICIT' },
+						numberDictionary: { type: 'number', dictionary: 'NUMBER_DICTIONARY' },
+						booleanDictionary: { type: 'boolean', dictionary: 'BOOLEAN_DICTIONARY' },
+						isoCalendarDateDictionary: {
+							type: 'ISOCalendarDate',
+							dictionary: 'ISO_CALENDAR_DATE_DICTIONARY',
+						},
+						isoTimeDictionary: { type: 'ISOTime', dictionary: 'ISO_TIME_DICTIONARY' },
+						isoCalendarDateTimeDictionary: {
+							type: 'ISOCalendarDateTime',
+							dictionary: 'ISO_CALENDAR_DATE_TIME_DICTIONARY',
+						},
+					},
+				},
+			);
+
+			const test1: Equals<
+				Filter<typeof schema>,
+				{
+					$and?: Filter<typeof schema>[];
+					$or?: Filter<typeof schema>[];
+					booleanOptional?: Condition<boolean>;
+					booleanRequired?: Condition<boolean>;
+					stringOptional?: Condition<string>;
+					stringRequired?: Condition<string>;
+					numberOptional?: Condition<number>;
+					numberRequired?: Condition<number>;
+					isoCalendarDateOptional?: Condition<ISOCalendarDate>;
+					isoCalendarDateRequired?: Condition<ISOCalendarDate>;
+					isoTimeOptional?: Condition<ISOTime>;
+					isoTimeRequired?: Condition<ISOTime>;
+					isoCalendarDateTimeOptional?: Condition<ISOCalendarDateTime>;
+					isoCalendarDateTimeRequired?: Condition<ISOCalendarDateTime>;
+					arrayOptional?: Condition<string>;
+					arrayRequired?: Condition<string>;
+					nestedArrayOptional?: Condition<string>;
+					nestedArrayRequired?: Condition<string>;
+					'embeddedOptional.innerEmbeddedProp'?: Condition<string>;
+					'embeddedRequired.innerEmbeddedProp'?: Condition<string>;
+					'documentArrayOptional.docStringProp'?: Condition<string>;
+					'documentArrayOptional.docNumberProp'?: Condition<number>;
+					'documentArrayRequired.docStringProp'?: Condition<string>;
+					'documentArrayRequired.docNumberProp'?: Condition<number>;
+					'documentArraySchemaOptional.docStringProp'?: Condition<string>;
+					'documentArraySchemaRequired.docStringProp'?: Condition<string>;
+					stringDictionary?: Condition<string>;
+					stringDictionaryExplicit?: Condition<string>;
+					numberDictionary?: Condition<number>;
+					booleanDictionary?: Condition<boolean>;
+					isoCalendarDateDictionary?: Condition<ISOCalendarDate>;
+					isoTimeDictionary?: Condition<ISOTime>;
+					isoCalendarDateTimeDictionary?: Condition<ISOCalendarDateTime>;
+					_id?: Condition<string>;
+				}
+			> = true;
+			expect(test1).toBe(true);
 		});
 	});
 });
