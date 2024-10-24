1445bc5b5a1a2138f5647603a9b2aca84b208df7
Shawn McKnight <smcknight@storis.com>
2024-10-24 16:00:39 -0400

    Add _originalRecordString to RequireModelMeta

--- src/compileModel.ts
+++ src/compileModel.ts
@@ -29,11 +29,11 @@ export type ModelConstructor<TSchema extends Schema | null> = ReturnType<
 
 export type Model<TSchema extends Schema | null> = InstanceType<ModelConstructor<TSchema>>;
 
-/** Used as an intersection type to make _id & __v properties required */
-export interface RequiredModelMeta {
-	_id: string;
-	__v: string;
-}
+type RequiredModelMetaProperties = '_id' | '__v' | '_originalRecordString';
+/** Used as an intersection type to make specific model properties required */
+export type RequiredModelMeta = {
+	[K in RequiredModelMetaProperties]: NonNullable<Model<Schema>[K]>;
+};
 
 /**
  * An intersection type that combines the `Model` class instance with the
